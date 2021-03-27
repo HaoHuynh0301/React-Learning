df4ce1342eee9dae781e9af7f61294c3a1bc6bb6
DESKTOP-7F19R05\Hao Huynh <hao152903@gmail.com>
2021-03-27 09:54:33 +0700

    commit

--- my-app-2/src/App.js
+++ my-app-2/src/App.js
@@ -1,40 +1,13 @@
 import logo from './logo.svg';
 import './css/styles.css'
 import { Component } from 'react';
-import ClickAction from './components/ClickAction.js'
-
-var lis = [1, 2, 3, 4];
-
-function Display(props) {
-  return (
-    <div>
-      <h1>Hello {props.Name}</h1>
-      <img src={props.imgPath}/>
-    </div>
-  );
-}
-
-class NewImage extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Hello {this.props.yourName}</h1>
-        <img className="card-img-bottom" src={this.props.imgPath}/>
-      </div>
-    );
-  }
-}
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Nmap/>
+        
       </div>
     );
   }
