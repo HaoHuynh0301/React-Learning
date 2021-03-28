17ac9472eef8c98b9b442be8064d816f32133e96
DESKTOP-7F19R05\Hao Huynh <hao152903@gmail.com>
2021-03-28 18:41:27 +0700

    react router

---
+++ my-app-2/src/components/ReactRouter.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class ReactRouter extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ReactRouter;
